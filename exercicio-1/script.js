ce52789c7853c9f0b876d60bcddf3803bcd8d54a
Guilherme Silva Ferreira <guilherme.silva.ferreira@outlook.pt>
2025-09-08 08:29:45 +0000

    Criado codico para apresentar imgens no exercicio1

--- exercicio-1/script.js
+++ exercicio-1/script.js
@@ -4,3 +4,27 @@ function horas() {
     var hora = agora.getHours()
     document.getElementById("texto").textContent = `Agora são ${hora} horas`
 }
+
+/*
+function imgem() {
+    var agora = new Date()
+    var hora = agora.getHours()
+    var img = document.getElementById("img")
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = "manha.png"
+        img.alt = "imagen que representa o dia"
+        document.body.style.background = "#e2cd9f"
+    } else if (hora >= 12 && hora <= 18) {
+        //boa tarde
+        img.src = "tarde.png"
+        img.alt = "imagen que representa o tarde"
+        document.body.style.background = "#b9846f"
+    } else {
+        //boa noite
+        img.src = "noite.png"
+        img.alt = "imagen que representa o noite"
+        document.body.style.background = "#515154"
+    }
+}
+*/
